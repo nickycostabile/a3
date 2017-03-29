9c6081bef70f8bec3b92028f08e9fd82f44e64cb
Nicky Costabile <nicolecostabile@gmail.com>
2017-03-29 15:15:03 -0400

    Fixed welcomecontroller issue in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-/* Laravel 5 Log Viewer Package for Local Env */
+Route::get('/', 'WelcomeController');
+
+/* Laravel 5 Log Viewer Package if Local Env */
 if(config('app.env') == 'local') {
 	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
